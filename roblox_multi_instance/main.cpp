ba269d7fe5523c9ef4940ab8d88699c0809d9eb5
George Davidson <georgedavidson392@gmail.com>
2023-10-22 18:40:43 -0400

    Add project files.

---
+++ roblox_multi_instance/main.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Roblox Multi Instance\n";
+	std::cout << "WARNING: All Roblox instances need to be closed before running this program. Otherwise, it will not work.\n";
+	CreateMutexA(NULL, TRUE, "ROBLOX_singletonMutex");
+	std::cout << "press enter to exit...";
+	std::cin.ignore();
+	return EXIT_SUCCESS;
+}
