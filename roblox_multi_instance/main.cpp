1d6fb16c554dfb3f029f9f33c44373dba63e7fbd
George Davidson <georgedavidson392@gmail.com>
2024-01-17 11:12:43 -0500

    updated main.cpp

--- roblox_multi_instance/main.cpp
+++ roblox_multi_instance/main.cpp
@@ -6,7 +6,7 @@ int main() {
     std::cout << "Roblox Multi Instance\n";
     std::cout << "WARNING: All Roblox instances need to be closed before running this program. Otherwise, it will not work.\n";
     CreateMutexA(NULL, TRUE, "ROBLOX_singletonMutex");
-    std::cout << "press enter to exit...";
+    std::cout << ">>> ";
     std::cin.ignore();
     return EXIT_SUCCESS;
 }
